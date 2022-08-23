78a6826010b52f89be6914ab914a944b422f384f
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-08-23 20:37:14 +0700

    commit fix homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
+import PatientLists from '../views/PatientLists.vue'
+import PatientMenuLayout from '@/views/event/PatientMenuLayout.vue'
+import NotFound from '@/views/NotFound.vue'
+import NetWorkError from '@/views/NetworkError.vue'
 import HomePage from '@/views/HomePage.vue'
 import NProgress from 'nprogress'
-import PatientLists from '../views/PatientLists.vue'
-
+import EventService from '@/services/EventService.js'
+import GStore from '@/store'
 const routes = [{
         path: '/',
         name: 'HomePage',
@@ -14,12 +18,60 @@ const routes = [{
         component: PatientLists,
         props: (route) => ({ page: parseInt(route.query.page) || 1 })
     },
-];
+    {
+        path: '/patient/:id',
+        name: 'PatientMenuLayout',
+        props: true,
+        component: PatientMenuLayout,
+        beforeEnter: (to) => {
+            return EventService.getPatient(to.params.id)
+                .then((response) => {
+                    GStore.patient = response.data
+                    GStore.patient.comments = GStore.comments.filter((comment) => GStore.patient.id == comment.IDpatient)
+                    console.log(GStore.patient.comments)
+                })
+                .catch((error) => {
+                    if (error.response && error.response.status == 404) {
+                        return {
+                            name: '404Resource',
+                            params: { resource: 'patient' }
+                        }
+                    } else {
+                        return { name: 'NetworkError' }
+                    }
+                })
+        },
+        children: []
+    },
+    {
+        path: '/404/:resource',
+        name: '404Resource',
+        component: NotFound,
+        props: true
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
+    },
+    {
+        path: '/network-error',
+        name: 'NetworkError',
+        component: NetWorkError
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
-});
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { top: 0 }
+        }
+    }
+})
 router.beforeEach(() => {
     NProgress.start()
 })
@@ -27,4 +79,4 @@ router.beforeEach(() => {
 router.afterEach(() => {
     NProgress.done()
 })
-export default router;
+export default router
